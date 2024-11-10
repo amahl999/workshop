51f7a8b0ace46a3750a9d1fe6f22d26b185f2bbe
Amahl999 <amahl@tutanota.com>
2024-11-10 22:02:51 +0100

    10NOV2024

---
+++ chat_bot.py
@@ -0,0 +1,24 @@
+def ask_name():
+    name = input("What is your name choom?").capitalize()
+    return name
+
+def calculation_expenses(expenses):
+    sum_total = sum(expenses)
+    return sum_total
+
+exp = [34.5, 67.6, 4598.34, 86.3]
+
+command = input("Which commans would you like to run fool? Options: expenses, ask_name ")
+
+if command == "expenses":
+    print (calculation_expenses(exp))
+
+elif command == "ask_name":
+    ask_name()
+
+else:
+    print("invalid command")
+
+
+#print(calculation_expenses(exp))
+#print(ask_name())
--- hello.py
+++ hello.py
@@ -3,5 +3,16 @@ print("Hello World")
 print(7 + 11)
 
 name = ("Patrick Persijn")
+name1 = ("Jackie Welles")
+name2 = input("What is your name choom?").capitalize()
+calculation = (45 * 23)
+decimal = 4.345 #float
 
-print(name)
+#boolean true or false
+
+
+print(name2)
+print(name1.lower())
+print(type(name))
+print(type(calculation))
+print(type(decimal))
