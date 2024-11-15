96075a098ab9813562e9d4bb80dd2b2f76d8568c
amahl999 <https://github.com/amahl999/workshop.git>
2024-11-15 23:34:05 +0100

    selectah

--- test.py
+++ test.py
@@ -3,6 +3,28 @@ while count < 8:
     print(count)
     count += 1
 
+def rekenmachine(machine):
+    sum_total = sum(machine)
+    return sum_total
 
-for fruit in ["apple", "lemon", "banana", "kiwi"]:
-    print(fruit)
+exp = [45.2, 732.01, 65, 78.4]
+
+print(rekenmachine(exp))
+
+
+
+def calculate_savings(savings):
+    saving_total = sum(savings)
+    return saving_total
+
+exp = []
+
+
+
+#for fruit in ["apple", "lemon", "banana", "kiwi"]:
+    #print(fruit)
+
+answer = input("What's your name dumbass?" ).capitalize()
+
+
+print(answer)
