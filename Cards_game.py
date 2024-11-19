e8eadc433466c5afca24d31bc3bac63596c11092
amahl999 <https://github.com/amahl999/workshop.git>
2024-11-19 22:35:27 +0100

    bullying

--- Cards_game.py
+++ Cards_game.py
@@ -8,25 +8,30 @@ Clubs = chr(9827)
 
 Backside = "backside"
 
+a = '''Welkom bij patrick's pesten spelletje.'''
+print(a)
+
 #Give the players 7 cards from the deck each
 
-def main():
-    intro = print("""Pesten kaartenspel, mede mogelijk gemaakt door ya boy""")
+def player_1():
+    intro = print("Player One:")
     return intro
+
+player_1()
 
 #for i in range (0, 6):
 
 
 def players():
-    players_cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, "King", "Queen", "Jack", "Joker"]
-    players_cards = print(random.choices(players_cards, k = 7))
+    ABI = [1, 2, 3, 4, 5, 6, 7, 8, 9, "King", "Queen", "Jack", "Joker"]
+    players_cards = print(random.choices(ABI, k = 7))
     return players_cards
 
-    
+players()
 
 #shuffle and divide
 def suit():
-        constants = ["Hearts", "Diamonds", "Spades", "Clubs"]
+        constants = [chr(9829), chr(9830), chr(9824), chr(9827)]
         constants =print(random.choice(constants))
         
 
@@ -35,10 +40,7 @@ def cards():
         my_list =print(random.choice(my_list))
         return my_list
         
-
-players()
 suit()
 cards()
 
-    
-
+bet = input("You first. Which card? ") 
